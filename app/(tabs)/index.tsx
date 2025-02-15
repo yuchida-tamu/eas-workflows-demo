66775fc04e8274d2b2749093e89b347fcdcce357
Yuta Uchida <yuchida4dev@gmail.com>
2025-02-15 09:33:07 +0900

    set up index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,114 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { useEffect } from "react";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import useSWR from "swr";
+import { ThemedText } from "../../components/ThemedText";
+import { ThemedView } from "../../components/ThemedView";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+type TravelLocation = {
+	id: number;
+	name: string;
+	description: string;
+	rating: number;
+	image_url: string;
+};
+
+const fetcher = (url: string) =>
+	fetch(url, {
+		method: "POST",
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Content-Type",
+		},
+	}).then((res) => res.json());
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	const { top, bottom } = useSafeAreaInsets();
+	const { data, error, isLoading } = useSWR(
+		"https://cdfvxxcscwbuproxmigr.supabase.co/functions/v1/demo-travel-location-list",
+		fetcher,
+	);
+
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
+	useEffect(() => {
+		console.log(error);
+	}, [error]);
+
+	useEffect(() => {
+		console.log(isLoading);
+	}, [isLoading]);
+
+	return (
+		<>
+			<ThemedView style={[styles.titleContainer, { paddingTop: top }]}>
+				<ThemedText type="title">Welcome to the Travel App!</ThemedText>
+			</ThemedView>
+			<ScrollView
+				style={{
+					paddingBottom: bottom,
+					paddingHorizontal: 16,
+				}}
+				contentContainerStyle={{ gap: 8 }}
+			>
+				{!isLoading &&
+					data &&
+					data.map((location: TravelLocation) => (
+						<ThemedView key={location.id} style={styles.itemContainer}>
+							<Image
+								source={{ uri: location.image_url }}
+								style={styles.itemImage}
+							/>
+							<View style={styles.itemBodyContainer}>
+								<ThemedText type="defaultSemiBold">{location.name}</ThemedText>
+								<ThemedText>{location.description}</ThemedText>
+								<View style={{ flexDirection: "row", alignItems: "center" }}>
+									<Ionicons name="star" size={16} color="orange" />
+									<ThemedText>{location.rating}</ThemedText>
+								</View>
+							</View>
+						</ThemedView>
+					))}
+			</ScrollView>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+	titleContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		padding: 16,
+		gap: 8,
+	},
+	itemContainer: {
+		gap: 8,
+		borderRadius: 8,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+	reactLogo: {
+		height: 200,
+		bottom: 0,
+		left: 0,
+		position: "absolute",
+	},
+	itemImage: {
+		height: 100,
+		borderTopLeftRadius: 8,
+		borderTopRightRadius: 8,
+	},
+	itemBodyContainer: {
+		paddingHorizontal: 8,
+		paddingVertical: 4,
+	},
 });
